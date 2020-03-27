e7568ca916e910ddf37dc5b4ef6bdba1d1872214
Josh Humphries <j.humphries@nhm.ac.uk>
2020-03-27 15:19:06 +0000

    Remove tiles because we're not using them

--- server.py
+++ server.py
@@ -68,19 +68,6 @@ def image_info(image_name):
         'height': height,
         'rights': 'https://creativecommons.org/publicdomain/zero/1.0/',
         'profile': 'level0',
-        # 'tiles': [
-        #     {
-        #         'width': 256,
-        #         'height': 256,
-        #         'scaleFactors': [
-        #             1,
-        #             2,
-        #             4,
-        #             8,
-        #             16,
-        #         ]
-        #     }
-        # ]
     })
 
 
