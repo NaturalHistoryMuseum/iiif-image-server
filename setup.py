a2c62d8d864c0a10906a4d5c00450313aaf04a03
Josh Humphries <j.humphries@nhm.ac.uk>
2020-05-06 11:45:05 +0100

    Fiddle with setup.py configuration
    Normal install can now be done through `pip install .`.
    Test deps can be installed by doing `pip install .[test]`.

--- setup.py
+++ setup.py
@@ -10,6 +10,20 @@ with open("README.md", "r") as readme_file:
 # whereas the latest commit on master does, hence we'll just install directly from github
 jpegtran_url = 'git+https://github.com/jbaiter/jpegtran-cffi.git@70928eb#egg=jpegtran-cffi'
 
+install_dependencies = [
+    f'jpegtran-cffi @ {jpegtran_url}',
+    'lru-dict~=1.1.6',
+    'pillow~=7.1.2',
+    'pyyaml~=5.3.1',
+    'tornado~=6.0.4',
+    'pycurl~=7.43.0.5',
+]
+test_dependencies = [
+    'pytest~=5.4.1',
+    'pytest-asyncio~=0.11.0',
+    'pytest-tornado~=0.8.0',
+]
+
 setup(
     name="iiif-image-server",
     version='0.0.1',
@@ -20,22 +34,11 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/NaturalHistoryMuseum/iiif-image-server/",
     packages=find_packages(),
-    install_requires=[
-        f'jpegtran-cffi @ {jpegtran_url}',
-        'lru-dict~=1.1.6',
-        'pillow~=7.1.2',
-        'pyyaml~=5.3.1',
-        'tornado~=6.0.4',
-        'pycurl~=7.43.0.5',
-    ],
-    dependency_links=[
-        jpegtran_url,
-    ],
-    tests_require=[
-        'pytest~=5.4.1',
-        'pytest-asyncio~=0.11.0',
-        'pytest-tornado~=0.8.0',
-    ],
+    install_requires=install_dependencies,
+    dependency_links=[jpegtran_url],
+    extras_require={
+        'test': test_dependencies,
+    },
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
